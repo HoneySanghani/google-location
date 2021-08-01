8a3921a2b9c617777be5467522dc9e4f213f7e2a
Honey Sanghani <honeysanghani98@gmail.com>
2021-08-01 16:14:28 -0400

    code to locate youself

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,8 +1,28 @@
 let map;
 
-function initMap() {
+function initMap(lat,lon) {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: lat, lng: lon},
     zoom: 8,
   });
+}
+
+
+var displayWeather=function(lat,lon){
+  var weatherApi="https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&units=imperial&APPID=a1ebf05a20a8fd712b4baf5c960acf21";
+  
+}
+var locate=function(){
+  if ("geolocation" in navigator){
+		navigator.geolocation.getCurrentPosition(function(position){ 
+			var currentLatitude = position.coords.latitude;
+			var currentLongitude = position.coords.longitude;
+      initMap(currentLatitude,currentLongitude);
+			// var infoWindowHTML = "Latitude: " + currentLatitude + "<br>Longitude: " + currentLongitude;
+			// var infoWindow = new google.maps.InfoWindow({map: map, content: infoWindowHTML});
+			// var currentLocation = { lat: currentLatitude, lng: currentLongitude };
+			// infoWindow.setPosition(currentLocation);
+			// document.getElementById("btnAction").style.display = 'none';
+		});
+	}
 }
